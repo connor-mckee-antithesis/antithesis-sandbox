d5bd14b46c1d3e33fb6dc2e4492e86a63a17c8cf
Clément Salaün <salaun.clement@gmail.com>
2024-05-03 01:36:07 +0200

    wip: workload loop
    Signed-off-by: Clément Salaün <salaun.clement@gmail.com>

--- workload/workload.go
+++ workload/workload.go
@@ -1,8 +1,12 @@
 package workload
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"time"
+
 	"github.com/antithesishq/antithesis-sdk-go/lifecycle"
 )
 
@@ -16,28 +20,33 @@ func (w *Workload) Execute() {
 
 	client := http.Client{}
 
-	req, err := http.NewRequest("POST", "http://server:8080/tests/1", nil)
+	for i := 0; i < 60; i++ {
+		log.Println(fmt.Sprintf("Executing workload request %d", i))
+		req, err := http.NewRequest("POST", "http://server:8080/tests/1", nil)
 
-	if err != nil {
-		panic(err)
-	}
+		if err != nil {
+			panic(err)
+		}
 
-	res, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
+		res, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		panic("expected status code 200")
-	}
+		if res.StatusCode != 200 {
+			panic("expected status code 200")
+		}
 
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
 
-	if string(b) != "1" {
-		panic("expected body to be 1")
+		if string(b) != "1" {
+			panic("expected body to be 1")
+		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
